3a04a297bdc774309675852351a340f3e7aeb567
DiptoPramanik <diptopramanikprogramming05@gmail.com>
2025-08-14 17:58:12 +0600

    function and recursive function

--- function.py
+++ function.py
@@ -56,6 +56,70 @@ def print_length(list):
 print_length(cities)
 print_length(fruits)
 
-# WAF to print 
+# WAF to print the elements of a list in a single line.
+def print_list(list):
+    for item in list:
+        print(item,end=" ")
 
+print_list(fruits)
+print()
+# WAF to find the factorial of n
+def factorial(n):
+    fact=1
+    for it in range(1,n+1):
+        fact*=it
+    return fact
 
+res = factorial(5)
+print(res)
+
+#WAF to convert USD to TAKA
+def usd_converter(dollar):
+    taka = dollar*121.58
+    return taka
+output = usd_converter(2)
+print("TAKA = ",output)
+
+#Recursion
+def show(n):
+    if (n==0):
+        return
+    print(n)
+    show(n-1)
+    # print("END")
+show(5)
+
+#Factorial
+def fact(n):
+    if(n==1 or n==0):
+        return 1
+    else:
+        return n*fact(n-1)
+    
+res = fact(5)
+print(res)
+
+#Write a recursive function to calculate the sum of first n natural numbers.
+def calc_sum(n):
+    if(n==1):
+        return 1
+    else:
+        return n+calc_sum(n-1)
+print(calc_sum(5))
+# # another way
+# def calc_sum(n):
+#     if(n==0):
+#         return 0
+#     return calc_sum(n-1) + n
+# sum  = calc_sum(5)
+# print(sum)
+
+#Write a recursive function to print all elements in a list
+def print_list(list,idx):
+    if(idx==len(list)):
+        return
+    print(list[idx])
+    print_list(list,idx+1)
+
+fruits =["mango","lichi","banana","cherry","apple"]
+print_list(fruits,0)
